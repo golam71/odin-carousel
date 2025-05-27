16c6e9b448ca5d1aed5ef4ffbbea00c963325bd8
golam71 <74001687+golam71@users.noreply.github.com>
2025-05-27 23:20:25 +0600

    ✨ Enhance image carousel with pagination and improved image transitions

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+let length;
 document.querySelectorAll(".swiper").forEach((item) => {
   // add pagination to all of them
-  let length = item.querySelectorAll("img").length;
+  length = item.querySelectorAll("img").length;
   for (let i = 0; i < length; i++) {
     let btn = document.createElement("button");
-    btn.class = i;
+    btn.className = i;
     item.querySelector(".pagination").appendChild(btn);
   }
 });
@@ -11,26 +12,87 @@ document.querySelectorAll(".swiper").forEach((item) => {
 let list = document.querySelectorAll(".swiper img");
 
 function showImage(image) {
-  image.classList.remove("swiper-hidden");
+  //   image.classList.remove("swiper-hidden");
+  image.classList.add("active-image");
 }
 
 function hideAllImage() {
   list.forEach((image) => {
-    image.classList.add("swiper-hidden");
+    // image.classList.add("swiper-hidden");
+    image.classList.remove("active-image");
   });
 }
 
 hideAllImage();
 showImage(list[0]);
 
+let number = 0;
+
 function next() {
-  for (let index = 0; index < list.length; index++) {
-    if (!list[index].classList.contains("swiper-hidden")) {
-      list[index].classList.add("swiper-hidden");
-      if (list[index + 1]) {
-        list[index + 1].classList.remove("swiper-hidden");
-      }
-      break;
-    }
+  number++;
+  if (number == length) {
+    number = 0;
   }
+  hideAllImage();
+  showImage(list[number]);
+  resetAllActiveButtons();
+  setButton(number);
 }
+
+function previous() {
+  number--;
+  if (number == -1) {
+    number = list.length - 1;
+  }
+  hideAllImage();
+  showImage(list[number]);
+  resetAllActiveButtons();
+  setButton(number);
+}
+
+document.querySelector(".next").onclick = () => {
+  next();
+};
+
+document.querySelector(".prev").onclick = () => {
+  previous();
+};
+
+function resetAllActiveButtons() {
+  document.querySelectorAll(".pagination button").forEach((btn) => {
+    btn.classList.remove("active");
+  });
+}
+
+function setButton(number) {
+  let btn = document.querySelectorAll(".pagination button")[number];
+  btn.classList.add("active");
+}
+
+document.querySelectorAll(".pagination button").forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    let number = e.target.classList[0];
+    hideAllImage();
+    showImage(list[number]);
+    resetAllActiveButtons();
+    e.target.classList.add("active");
+  });
+});
+
+let intervalId;
+
+function startLogging() {
+  intervalId = setInterval(() => {
+    next();
+    console.log(1);
+  }, 2000);
+}
+
+function stopLogging() {
+  clearInterval(intervalId);
+}
+
+startLogging();
+
+document.querySelector(".swiper").addEventListener("mouseover", stopLogging);
+document.querySelector(".swiper").addEventListener("mouseleave", startLogging);
